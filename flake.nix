64d76360e0cb3758a5b1c5ece29bb664da8f5603
hugosenari <hugosenari@gmail.com>
2022-03-12 03:56:15 -0300

    chore: re index persistent_enums

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Define enums which values preserve their binary representation upon inserting or reordering'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."persistent_enums-master".url = "path:./master";
+  inputs."persistent_enums-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."persistent_enums-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
